0fce8e7b5b372573f9470183172a3b4fbbff6593
sahaj <sahaj@DESKTOP-N8JCCMG>
2025-04-14 19:58:08 +0530

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebBasketDev.Startup))]
+namespace WebBasketDev
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
