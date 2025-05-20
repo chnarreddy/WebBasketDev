c76c165dd1cabb68b918c023128a441c557d4f46
Sahaja Sree Foods <sahajasreefoods@gmail.com>
2025-05-20 19:35:45 +0530

    Added User Login Page.

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebBasketDev
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Customers", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "User", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
